7837098e06efe85aff60d277c5e7ada44694f23e
Kumuditha <kumudithasilva66@gmail.com>
2025-06-19 16:45:13 +0530

    add  componet directory

--- temp.py
+++ temp.py
@@ -13,6 +13,7 @@ list_of_folders = [
     f"{project_name}/models/__init__.py", 
     f"{project_name}/utils/__init__.py",
     f"{project_name}/config/__init__.py",
+    f"{project_name}/components/__init__.py",
     f"{project_name}/config/configuration.py",
     f"{project_name}/pipeline/__init__.py",
     f"{project_name}/entity/__init__.py",
