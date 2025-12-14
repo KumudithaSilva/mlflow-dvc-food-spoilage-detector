39d9da7be0fe13a6a70ecb5e6c775ee28cdc2259
Kumuditha <kumudithasilva66@gmail.com>
2025-12-14 20:01:42 +0530

    add image_utils.py with image resizing and saving functions

---
+++ src/utils/image_utils.py
@@ -0,0 +1,54 @@
+import os
+from box.exceptions import BoxValueError
+import numpy as np
+from utils import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+import base64
+import yaml
+from PIL import Image
+from typing import Sequence
+
+
+@ensure_annotations
+def reshape_image(image_path: Path,  image_size: tuple) -> Image.Image:
+    """
+    Resizes an image to a fixed size and ensures 3 RGB channels.
+
+    Args:
+        image_path (Path): Path to the input image
+        image_size (tuple): Desired image size (width, height)
+
+    Returns:
+        PIL.Image.Image: Resized RGB image
+    """
+    try:
+        img = Image.open(image_path).convert("RGB")
+        img = img.resize((image_size), Image.BILINEAR)
+        return img
+    except Exception:
+        logger.exception(f"Failed to resize image: {image_path}")
+        raise
+
+# @ensure_annotations
+def save_image(image: Image.Image, save_path: Path) -> None:
+    """
+    Saves a PIL Image to the specified path.
+
+    Args:
+        image (PIL.Image.Image): Image to be saved
+        save_path (Path): Path where the image will be saved
+
+    Raises:
+        Exception: If saving the image fails
+    """
+    try:
+        save_path.parent.mkdir(parents=True, exist_ok=True)
+        image.save(save_path)
+    except Exception:
+        logger.exception(f"Failed to Save image to: {save_path}")
+        raise
