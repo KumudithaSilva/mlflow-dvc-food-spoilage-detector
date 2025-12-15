9cddc3ecec35bb49b32f8cdf2288b8309851c952
Kumuditha <kumudithasilva66@gmail.com>
2025-12-15 21:55:37 +0530

    style: reorder imports and improve code formatting for consistency

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,28 +1,33 @@
-from entity.config_entity import DataIngestionConfig
 import os
 import zipfile
-import rarfile
+
 import gdown
+import rarfile
+
+from entity.config_entity import DataIngestionConfig
 from utils import logger
+
 
 class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
         self.config = config
 
-    def download_file(self)-> str:
+    def download_file(self) -> str:
         try:
             dataset_url = self.config.source_URL
             zip_donwload_dir = self.config.local_datafile
             os.makedirs("artifacts/data_ingestion", exist_ok=True)
-            logger.info(f"Donwloaded data from {dataset_url} into file {zip_donwload_dir}")
+            logger.info(
+                f"Donwloaded data from {dataset_url} into file {zip_donwload_dir}"
+            )
 
             file_id = dataset_url.split("/")[-2]
             prefix_url = "https://drive.google.com/uc?/export=download&id="
-            gdown.download(prefix_url+file_id, zip_donwload_dir)
-        
+            gdown.download(prefix_url + file_id, zip_donwload_dir)
+
         except Exception as e:
             raise e
-    
+
     def extract_zip_file(self):
         unzip_path = self.config.unzip_dir
         os.makedirs(unzip_path, exist_ok=True)
@@ -35,4 +40,6 @@ class DataIngestion:
             with rarfile.RarFile(file_path) as rar_ref:
                 rar_ref.extractall(unzip_path)
         else:
-            raise Exception("File format not supported for extraction. Only .zip and .rar are supported.")
+            raise Exception(
+                "File format not supported for extraction. Only .zip and .rar are supported."
+            )
