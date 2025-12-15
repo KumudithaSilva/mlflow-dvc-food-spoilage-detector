e6524a5d46b6b92c0344fc0fdf8a7a4da60c9fcd
Kumuditha <kumudithasilva66@gmail.com>
2025-12-15 21:54:51 +0530

    style: reorder imports and format setup.py for consistency

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 """
 
@@ -22,13 +22,13 @@ setup(
     description="Early stage food spoilage detection",
     long_description=open("README.md").read(),
     long_description_content_type="",
-    maintainer= "kumuditha",
-    maintainer_email= "kumudithasilva66@gmail.com",
-    url= "https://github.com/KumudithaSilva/mlflow-dvc-food-spoilage-detector",
-    project_url ={
-        "Bug_Tracker" : "https://github.com/KumudithaSilva/mlflow-dvc-food-spoilage-detector/issues"
+    maintainer="kumuditha",
+    maintainer_email="kumudithasilva66@gmail.com",
+    url="https://github.com/KumudithaSilva/mlflow-dvc-food-spoilage-detector",
+    project_url={
+        "Bug_Tracker": "https://github.com/KumudithaSilva/mlflow-dvc-food-spoilage-detector/issues"
     },
     package_dir={"": "src"},
-    packages= find_packages(where="src"),
+    packages=find_packages(where="src"),
     python_requires='>=3.11, <3.12',
 )
