5e5fc5ff0b4ec862e5b85a5cd9429a8fee914aa8
Kumuditha <kumudithasilva66@gmail.com>
2025-12-15 21:56:33 +0530

    style: improve code formatting and consistency in config_entity.py

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -1,21 +1,31 @@
 from dataclasses import dataclass
 from pathlib import Path
 
+
 @dataclass(frozen=True)
 class DataIngestionConfig:
-   root_dir: Path
-   source_URL: str
-   local_datafile: Path
-   unzip_dir: Path
+    root_dir: Path
+    source_URL: str
+    local_datafile: Path
+    unzip_dir: Path
+
+
+@dataclass(frozen=True)
+class DataPreprocessingConfig:
+    root_dir: Path
+    unzip_dir: Path
+    reshape_dir: Path
+    image_size: tuple
+
 
 @dataclass(frozen=True)
 class PrepareBaseModelConfig:
-   root_dir: Path
-   base_model_path: Path
-   updated_base_model_path: Path
-   updated_base_model_image_path: Path
-   params_image_size: list
-   params_learning_rate: float
-   params_include_top: bool
-   params_weights: str
-   params_classes: int
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    updated_base_model_image_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
