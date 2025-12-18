1b3462388386efc822f1c9ca9fa9574c83af269e
Kumuditha <kumudithasilva66@gmail.com>
2025-12-18 21:55:56 +0530

    style: reformat import statement for PrepareBaseModelTraningPipeline in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pipeline.stage_01_data_ingestion import DataIngestionTraningPipeline
 from pipeline.state_02_data_preprocess import DataPreprocessTraningPipeline
-from pipeline.state_03_prepare_base_model import PrepareBaseModelTraningPipeline
+from pipeline.state_03_prepare_base_model import \
+    PrepareBaseModelTraningPipeline
 from utils import logger
 
 STAGE_NAME = "Data Ingestion Stage"
