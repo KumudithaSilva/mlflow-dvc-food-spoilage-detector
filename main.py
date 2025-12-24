aad86102eea2890d5fd22a970292d333fc067b3e
Kumuditha <kumudithasilva66@gmail.com>
2025-12-24 22:19:48 +0530

    feat: add model training stage to the pipeline

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pipeline.stage_01_data_ingestion import DataIngestionTraningPipeline
 from pipeline.state_02_data_preprocess import DataPreprocessTraningPipeline
 from pipeline.state_03_prepare_base_model import \
     PrepareBaseModelTraningPipeline
+from pipeline.state_04_model_training import TraningPipeline
 from utils import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -35,3 +36,14 @@ try:
     logger.info(f">>>>> STAGE {STAGE_NAME} COMPLETED <<<<< \n\n")
 except Exception as e:
     logger.error(f">>>>> STAGE {STAGE_NAME} ERROR {e} <<<<< \n\n")
+
+
+STAGE_NAME = "Model Training Stage"
+
+try:
+    logger.info(f">>>>> STAGE {STAGE_NAME} STARTED <<<<<")
+    train_base_model = TraningPipeline()
+    train_base_model.main()
+    logger.info(f">>>>> STAGE {STAGE_NAME} COMPLETED <<<<< \n\n")
+except Exception as e:
+    logger.exception(f">>>>> STAGE {STAGE_NAME} ERROR {e} <<<<< \n\n")
