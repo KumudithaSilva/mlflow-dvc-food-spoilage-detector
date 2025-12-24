74d6c8e83e2e2fd9720c1bb6139401a77ebf4f7f
Kumuditha <kumudithasilva66@gmail.com>
2025-12-24 22:26:01 +0530

    refactor: improve import formatting for consistency in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pipeline.stage_01_data_ingestion import DataIngestionTraningPipeline
 from pipeline.state_02_data_preprocess import DataPreprocessTraningPipeline
-from pipeline.state_03_prepare_base_model import PrepareBaseModelTraningPipeline
+from pipeline.state_03_prepare_base_model import \
+    PrepareBaseModelTraningPipeline
 from pipeline.state_04_model_training import TraningPipeline
 from utils import logger
 
