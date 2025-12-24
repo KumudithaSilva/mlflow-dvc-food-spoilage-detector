e2b1045c75b0963b540f335004f6975003df8a1e
Kumuditha <kumudithasilva66@gmail.com>
2025-12-24 22:25:01 +0530

    refactor: improve import formatting and ensure consistent style in configuration.py

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -2,8 +2,12 @@ import os
 from pathlib import Path
 
 from constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
-from entity.config_entity import (DataIngestionConfig, DataPreprocessingConfig,
-                                  PrepareBaseModelConfig, TrainingConfig)
+from entity.config_entity import (
+    DataIngestionConfig,
+    DataPreprocessingConfig,
+    PrepareBaseModelConfig,
+    TrainingConfig,
+)
 from utils.base_utils import create_directories, read_yaml
 
 
