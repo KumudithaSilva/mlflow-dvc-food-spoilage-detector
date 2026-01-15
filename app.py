eade557aa4835971790a76b785dff70bc5e06b0c
Kumuditha Silva <kumudithasilva66@gmail.com>
2026-01-15 19:23:57 +0530

    integrated streamlit to fastapi

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from pathlib import Path
+
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 from pydantic import BaseModel
 from pipeline.stage_06_prediction import PredictionPipeline
 from utils.image_utils import decodeImage
@@ -7,6 +11,15 @@ import traceback
 
 # FastAPI app
 app = FastAPI(title="Food Spoilage Detector")
+
+# Allow CORS for Streamlit requests
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Image storage path
 TEMP_IMAGE_PATH = Path("prediction_data/inputImage.png")
 # Load Model
@@ -17,6 +30,11 @@ pipeline = PredictionPipeline([TEMP_IMAGE_PATH])
 class ImageRequest(BaseModel):
     image: str
 
+
+@app.get("/")
+def root():
+    # Redirect user to Streamlit
+    return RedirectResponse("http://localhost:8501")
 
 
 @app.post("/predict")
