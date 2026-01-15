1e7bf73202f07c9cf044446cc17b468aeaf6a099
Kumuditha Silva <kumudithasilva66@gmail.com>
2026-01-15 23:09:05 +0530

    modify prediction pipeline with removing image paths

--- src/pipeline/stage_06_prediction.py
+++ src/pipeline/stage_06_prediction.py
@@ -6,37 +6,33 @@ from utils import logger
 STAGE_NAME = "Prediction Stage"
 
 class PredictionPipeline:
-    def __init__(self, image_paths: list[Path]):
-        self.image_paths = image_paths
-
-    def main(self):
-        try:
-            # Initilize the ConfigurationManager
+    def __init__(self):
+            # Initialize the ConfigurationManager
             config_manager = ConfigurationManager()
             # Get the prediction config
-            pred_config = config_manager.get_prediction_config()
+            self.pred_config = config_manager.get_prediction_config()
             # Initialize the Prediction class
-            predictor = Prediction(config=pred_config)
-            # Perform predictions
-            results = predictor.predict(self.image_paths)
+            self.predictor = Prediction(config=self.pred_config)
+    
 
-            return results
-
-        except Exception as e:
-            raise e
-
+    def run(self, image_paths: list[Path]):
+        """
+        Run prediction on provided images
+        """
+        return self.predictor.predict(image_paths)
+    
 
 if __name__ == "__main__":
     try:
         logger.info(f">>>>> STAGE {STAGE_NAME} STARTED <<<<<")
 
         images = [
-            Path("prediction_data/p1.png"),
-            Path("prediction_data/p2.png")
+            Path("object_store/prediction_data/p1.png"),
+            Path("object_store/prediction_data/p2.png")
         ]
 
-        pipeline = PredictionPipeline(images)
-        output = pipeline.main()
+        pipeline = PredictionPipeline()
+        output = pipeline.run(images)
 
         logger.info(f"Prediction Output: {output}")
         logger.info(f">>>>> STAGE {STAGE_NAME} COMPLETED <<<<< \n\n")
