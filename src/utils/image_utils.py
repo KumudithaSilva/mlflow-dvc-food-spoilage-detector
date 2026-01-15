e9252dc16ea13233a332ed7f77d568c68f78f6da
Kumuditha Silva <kumudithasilva66@gmail.com>
2026-01-15 18:24:16 +0530

    add decorder

--- src/utils/image_utils.py
+++ src/utils/image_utils.py
@@ -2,6 +2,9 @@ from pathlib import Path
 
 from ensure import ensure_annotations
 from PIL import Image
+from io import BytesIO
+
+import base64
 
 from utils import logger
 
@@ -45,3 +48,34 @@ def save_image(image: Image.Image, save_path: Path) -> None:
     except Exception:
         logger.exception(f"Failed to Save image to: {save_path}")
         raise
+
+# @ensure_annotations
+def decodeImage(imageData: str, save_path: Path) -> None :
+    """
+    Decodes a base64 encoded image and saves it to a file.
+
+    Args:
+        imageData (str): Base64 encoded image data
+        save_path (Path): Path where the image will be saved
+    
+    Raises:
+        Exception: If decoding or saving fails    
+    """
+    try:
+        save_path.parent.mkdir(parents=True, exist_ok=True)
+
+        # Remove prefix
+        if "," in imageData:
+            imageData = imageData.split(",")[1]
+
+        # Decode base64 to byte
+        image_byte = base64.b64decode(imageData)
+        # Convert byte to PIL Image
+        img = Image.open(BytesIO(image_byte))
+        # Save the image
+        img.save(save_path)
+        logger.info(f"Image successfully saved at {save_path}")
+    except Exception as e:
+        logger.exception(f"Failed to decode and save image at {save_path}")
+        raise e
+        
