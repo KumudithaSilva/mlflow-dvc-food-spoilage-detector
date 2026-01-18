b83916640ff6ddd362275ade4fc405d71a46f78c
Kumuditha Silva <kumudithasilva66@gmail.com>
2026-01-18 16:30:31 +0530

    add aws s3 and configure save byte files

--- app.py
+++ app.py
@@ -4,20 +4,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from pydantic import BaseModel
-from pipeline.stage_06_prediction import PredictionPipeline
-from utils.image_utils import decodeImage
+from pipeline.stage_07_prediction import PredictionPipeline
+from utils.image_utils import decodeImageToPNGBytes, saveBytesToFile
 import traceback
 import os 
 import uuid
+from utils.s3_utils import S3Client
 
-
-# ------------------------------------------------------------
-# Configuration
-# ------------------------------------------------------------
-
-OBJECT_STORE_ROOT = Path(os.getenv("OBJECT_STORE_ROOT", "object_store"))
-UPLOAD_DIR = OBJECT_STORE_ROOT / "prediction_data"
-UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
 
 # ------------------------------------------------------------
 # FastAPI App
@@ -34,11 +27,25 @@ app.add_middleware(
 )
 
 # ------------------------------------------------------------
+# Temporally File location
+# ------------------------------------------------------------
+
+UPLOAD_DIR = Path(os.getenv("OBJECT_STORE_ROOT", "temp"))
+UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
+
+# ------------------------------------------------------------
+# S3 Config
+# ------------------------------------------------------------
+
+S3_BUCKET = os.getenv("S3_BUCKET", "food-spoilage-ml")
+S3_BUCKET_DIR = os.getenv("S3_BUCKET_DIR", "user-uploads")
+s3_client = S3Client() 
+
+# ------------------------------------------------------------
 # Load Prediction Pipeline ONCE
 # ------------------------------------------------------------
 
 prediction_pipeline = PredictionPipeline()
-
 
 # ------------------------------------------------------------
 # Request Schema
@@ -46,7 +53,6 @@ prediction_pipeline = PredictionPipeline()
 
 class ImageRequest(BaseModel):
     image: str
-
 
 # ------------------------------------------------------------
 # Prediction Endpoint
@@ -56,19 +62,27 @@ class ImageRequest(BaseModel):
 def predict(data: ImageRequest):
     """
     1. Generate unique object key
-    2. Save image to object storage
+    2. Save image to object storage and S3
     3. Pass path to prediction pipeline
     """
     try:
         # Generate unique object name
         image_name = f"{uuid.uuid4()}.png"
-        image_path = UPLOAD_DIR / image_name
-        
-        # decode base64
-        decodeImage(data.image, image_path)
 
+        image_path = UPLOAD_DIR / image_name
+        s3_key = f"{S3_BUCKET_DIR}/{image_name}"
+        
+        # Decode base64
+        file_obj = decodeImageToPNGBytes(data.image)
+
+        # Writes a BytesIO object to given path
+        saveBytesToFile(file_obj, image_path)
+
+        # Upload to S3
+        s3_client.upload_fileobj(file_obj, S3_BUCKET, s3_key)
+    
         # run prediction
-        result = prediction_pipeline.run([image_path])
+        result = prediction_pipeline.main([image_path])
 
         return result
     
